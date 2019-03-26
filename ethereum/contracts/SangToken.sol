7f7bde08595c3021284a27940d1caec9f4684ceb
Thananjaya <thananjaya091993@gmail.com>
2019-03-26 18:37:16 +0530

    added token sale contract and its test cases

--- ethereum/contracts/SangToken.sol
+++ ethereum/contracts/SangToken.sol
@@ -2,6 +2,7 @@ pragma solidity^0.4.17;
 
 contract SangToken {
     uint public totalSupply;
+    address public owner;
     string public name = 'Sang Token';
     string public symbol = 'SANG';
     mapping(address => uint) public balanceOf;
@@ -20,19 +21,20 @@ contract SangToken {
     );
 
     modifier valueCheck(uint _value){
-        require(balanceOf[msg.sender] >= _value);
+        require(balanceOf[owner] >= _value);
         _;
     }
 
     constructor(uint initialSupply) public {
-        balanceOf[msg.sender] = initialSupply;
+        owner = msg.sender;
+        balanceOf[owner] = initialSupply;
         totalSupply = initialSupply;
     }
 
     function transfer(address _to, uint _value) public valueCheck(_value) returns(bool success) {
-        balanceOf[msg.sender] -= _value;
+        balanceOf[owner] -= _value;
         balanceOf[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
+        emit Transfer(owner, _to, _value);
         return true;
     }
 
@@ -53,5 +55,55 @@ contract SangToken {
         
         emit Transfer(_from, _to, _value);
         return true;
+    }
+}
+
+contract SangTokenSale {
+    SangToken public sangToken;
+    uint public tokensForSale;
+    uint public tokenPrice;
+    uint public tokensSold;
+    address public admin;
+
+    constructor(address tokenAddress) public {
+        sangToken = SangToken(tokenAddress);
+        admin = msg.sender;
+    }
+
+    modifier adminAccess() {
+        require(msg.sender == admin);
+        _;
+    }
+
+    event Sell(
+        address indexed sender,
+        uint tokens
+    );
+
+    function setTokenPrice(uint price) public adminAccess {
+        tokenPrice = price;
+    }
+
+    function setNumberOfTokensForSale (uint totalTokensForSale) public adminAccess {
+        require(totalTokensForSale <= sangToken.totalSupply());
+        require(sangToken.transfer(admin, totalTokensForSale));
+        tokensForSale += totalTokensForSale;
+    }
+
+    function buyTokens(uint totalTokens) public payable {
+        require(totalTokens <= tokensForSale);
+        require(msg.value >= totalTokens * tokenPrice);
+        require(sangToken.transfer(msg.sender, totalTokens));
+
+        emit Sell(msg.sender, totalTokens);
+
+        tokensSold += totalTokens;
+        tokensForSale -= totalTokens;
+    }
+
+    function endTokenSale() public adminAccess {
+        require(sangToken.transfer(admin, tokensForSale));
+
+        selfdestruct(admin);
     }
 }
--- ethereum/test/sangToken.test.js
+++ ethereum/test/sangToken.test.js
@@ -2,11 +2,15 @@ const assert = require('assert');
 const Web3 = require('web3');
 const ganache = require('ganache-cli');
 const web3 = new Web3(ganache.provider());
-const compiledContract = require('../build/SangToken.json'); 
+const compiledContract = require('../build/SangToken.json');
+const compiledTokenSale = require('../build/SangTokenSale.json');
 
 let accounts;
 let sangToken;
-const initialSupply = 100;
+let sangTokenAddress;
+let sangTokenSale;
+const initialSupply = 100000;
+const tokenPrice = web3.utils.toWei('0.01', 'ether');
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
@@ -14,6 +18,16 @@ beforeEach(async () => {
         .deploy({
             data: compiledContract.bytecode,
             arguments: [initialSupply]
+        })
+        .send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+    sangTokenAddress = sangToken.options.address;
+    sangTokenSale = await new web3.eth.Contract(JSON.parse(compiledTokenSale.interface))
+        .deploy({
+            data: compiledTokenSale.bytecode,
+            arguments: [sangTokenAddress]
         })
         .send({
             from: accounts[0],
@@ -67,5 +81,75 @@ describe('test cases for SangToken ERC 20 token', () => {
             assert.fail(err);
         }
     });
+});
+
+describe('tests for token sale', async () => {
+    it('tests whether the contract been deployed successfully', () => {
+        assert.ok(sangTokenSale.options.address);
+    });
+
+    it('tests whether the admin been successfully created', async () => {
+        const adminAddress = await sangTokenSale.methods.admin().call();
+        assert.equal(adminAddress, accounts[0]);
+    });
+
+    it('tests for setting token price in wei', async () => {
+       await sangTokenSale.methods.setTokenPrice(1000).send({ 
+           from: accounts[0],
+           gas: '100000'
+       });
+       const price = await sangTokenSale.methods.tokenPrice().call();
+       assert.equal(price, 1000); 
+    });
+
+    it('test for setting the total tokens for sale', async () => {
+        await sangTokenSale.methods.setNumberOfTokensForSale(500).send({
+            from: accounts[0],
+            gas: '100000'
+        });
+        const totalTokens = await sangTokenSale.methods.tokensForSale().call();
+        assert.equal(totalTokens, 500)
+    });
+
+    it ('test for setting the price for each token', async () => {
+        await sangTokenSale.methods.setTokenPrice(tokenPrice).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+        const price = await sangTokenSale.methods.tokenPrice().call();
+        assert.equal(price, tokenPrice); 
+    });
+
+    it('test for buying an token by another user', async() => {
+        try {
+            await sangTokenSale.methods.setNumberOfTokensForSale(500).send({
+                from: accounts[0],
+                gas: '100000'
+            });
+            await sangTokenSale.methods.setTokenPrice(tokenPrice).send({
+                from: accounts[0],
+                gas: '1000000'
+            });
+            await sangTokenSale.methods.buyTokens(10).send({
+                from: accounts[1],
+                value: web3.utils.toWei('0.1', 'ether'),
+                gas: '1000000'
+            });
+        } catch(err) {
+            assert.fail(err);
+        }
+    });
+
+    it('tests for ending the token sale', async() => {
+        try {
+            await sangTokenSale.methods.endTokenSale().send({
+                from: accounts[0],
+                gas: '1000000'
+            });
+        } catch(err) {
+            assert.fail(err);
+        }
+    })
+
 });
 
